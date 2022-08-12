57dc832fa5d7f4a1abc67fa25fb61ed04ec1fad5
Ivan Duran <iamivanduran02@gmail.com>
2022-08-12 16:19:06 -0400

    added react-router-dom and setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
-import { motion, AnimatePresence, animate } from "framer-motion";
-import { useEffect } from "react";
-import { useState } from "react";
-import Modal from "./components/Modal/index";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ModalScreen from "./screens/Modal";
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const close = () => setModalOpen(false);
-  const open = () => setModalOpen(true);
-
-  const [x, setX] = useState(0);
-  const [toggle, setToggle] = useState(false);
-
-  const handleClick = () => {
-    modalOpen ? close() : open();
-
-    setToggle(!toggle);
-    if (toggle) setX(0);
-    else setX(100);
-  };
-
-  // useEffect(() => {});
   return (
-    <div className="flex justify-center items-center">
-      <motion.button
-        className="save-button"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 1 }}
-        animate={{ x: x }}
-        onClick={() => handleClick()}
-      >
-        Launch modal
-      </motion.button>
-
-      <AnimatePresence
-        // Disable any intial animations on children that
-        // are present when the component is first rendered.
-        initial={false}
-        // Only render one component at a time.
-        // The exiting component will finish its exit
-        // animation before entering component is rendered
-        exitBeforeEnter={true}
-        // Fires wehn all exiting nodes have completed animating out
-        onExitComplete={() => null}
-      >
-        {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
-      </AnimatePresence>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/modal" element={<ModalScreen />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/screens/Modal.js
@@ -0,0 +1,52 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import Modal from "../components/Modal";
+
+function ModalScreen() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+
+  const [x, setX] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    modalOpen ? close() : open();
+
+    setToggle(!toggle);
+    if (toggle) setX(0);
+    else setX(100);
+  };
+
+  // useEffect(() => {});
+  return (
+    <div className="flex justify-center items-center">
+      <motion.button
+        className="save-button"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 1 }}
+        animate={{ x: x }}
+        onClick={() => handleClick()}
+      >
+        Launch modal
+      </motion.button>
+
+      <AnimatePresence
+        // Disable any intial animations on children that
+        // are present when the component is first rendered.
+        initial={false}
+        // Only render one component at a time.
+        // The exiting component will finish its exit
+        // animation before entering component is rendered
+        exitBeforeEnter={true}
+        // Fires wehn all exiting nodes have completed animating out
+        onExitComplete={() => null}
+      >
+        {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default ModalScreen;
